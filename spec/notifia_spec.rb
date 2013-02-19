1547aabff21198431380d4ad77984fed8dc45652
Michael Erasmus <michaelerasmus@gmail.com>
2013-02-20 00:18:06 +0200

    Fix spec describe block

--- spec/notifia_spec.rb
+++ spec/notifia_spec.rb
@@ -4,22 +4,20 @@ describe "notifia" do
     @hn.extend Notifia
   end
 
-  describe 'on' do
-    it "exists" do
-      @hn.should.respond_to :on
-    end
-    
-    it 'observes notifications' do
-      @hn.on :bla {|object| observed = true} 
-      @hn.should.observes :bla 
-    end
+  it "has a 'on' method" do
+    @hn.should.respond_to :on
+  end
+  
+  it 'observes notifications' do
+    @hn.on :bla {|object| observed = true} 
+    @hn.should.observes :bla 
+  end
 
-    it 'fires notifications' do
-      fired = false
-      @hn.on :foo {|object| fired = true} 
-      @hn.fire :foo
-      fired.should == true
-    end
+  it 'fires notifications' do
+    fired = false
+    @hn.on :foo {|object| fired = true} 
+    @hn.fire :foo
+    fired.should == true
   end
 end
 
