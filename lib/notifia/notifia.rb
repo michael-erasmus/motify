1104002c858b8bacb3c8e63e6965fb43735c93a6
Michael Erasmus <michaelerasmus@gmail.com>
2013-02-19 23:52:30 +0200

    A little refactoring

--- lib/notifia/notifia.rb
+++ lib/notifia/notifia.rb
@@ -1,5 +1,19 @@
 module Notifia
+
   def on(state,&block)
-    NSNotificationCenter.defaultCenter.addObserverForName(state, object:nil, queue:NSOperationQueue.mainQueue, usingBlock:block)
+    add_observer(state,nil,&block)
+  end
+
+  private
+  def add_observer(state, object, &block)
+    default_center.addObserverForName(state, object:nil, queue:main_queue, usingBlock:block)
+  end
+
+  def default_center
+    NSNotificationCenter.defaultCenter
+  end
+
+  def main_queue
+    NSOperationQueue.mainQueue
   end
 end
